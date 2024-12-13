1823794b543b00cb0ba839f8c3856a2562d79b7e
neo-rabbit <diogo.m.guedes@ua.pt>
2024-12-13 15:46:18 +0000

    product details

--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 export const routes: Routes = [
 
     { path: '', component: ProductListComponent }, // Página inicial
+    { path: 'product/:slug', component: ProductDetailsComponent },
 ];
---
+++ angular-app/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../services/api.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [CommonModule],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent {
+  product: any = null;
+  reviews: any[] = []
+  slug: string = ''
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+  
+  ngOnInit(): void {
+    this.slug = this.route.snapshot.params['slug'];
+    this.apiService.getProduct(this.slug).subscribe((data) => {
+      this.product = data;
+    });
+  }
+}
